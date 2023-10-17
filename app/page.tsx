4743ad6c7f4d58e912c0dc6db6f93ab8ced45ee7
NIHAL100102 <106856711+NIHAL100102@users.noreply.github.com>
2023-10-17 11:28:57 +0530

    website

---
+++ app/page.tsx
@@ -0,0 +1,86 @@
+import Image from "next/image";
+import Me from "@/public/nihal.png";
+
+export default function Home() {
+  return (
+    <div className="divide-y divide-gray-100 dark:divide-gray-700">
+      <div className="space-y-2 pt-5 pb-8 md:space-x-5">
+        <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-13">
+          Home
+        </h1>
+      </div>
+
+      <div className="items-center space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
+        <div className="flex flex-col items-center pt-8">
+          <Image
+            alt="Picture of jan marshal"
+            src={Me}
+            className="h-120 w-100 rounded-full object-cover object-top"
+          />
+          <h3 className="pt-8 pb-4 text-2xl font-extrabold leading-8 tracking-tight">
+            NIHAL DEVADIGA
+          </h3>
+          <p className="text-gray-500 dark:text-gray-300 text-center font-mono">
+            Social Media Accounts ↓
+          </p>
+
+          <div className="flex space-x-5 pt-6">
+            <a href="https://github.com/NIHAL100102" target="_blank">
+              <svg
+                viewBox="0 0 1024 1024"
+                fill="currentColor"
+                className="w-8 h-8 text-black dark:bg-white hover:text-teal-600"
+              >
+                <path d="M511.6 76.3C264.3 76.2 64 276.4 64 523.5 64 718.9 189.3 885 363.8 946c23.5 5.9 19.9-10.8 19.9-22.2v-77.5c-135.7 15.9-141.2-73.9-150.3-88.9C215 726 171.5 718 184.5 703c30.9-15.9 62.4 4 98.9 57.9 26.4 39.1 77.9 32.5 104 26 5.7-23.5 17.9-44.5 34.7-60.8-140.6-25.2-199.2-111-199.2-213 0-49.5 16.3-95 48.3-131.7-20.4-60.5 1.9-112.3 4.9-120 58.1-5.2 118.5 41.6 123.2 45.3 33-8.9 70.7-13.6 112.9-13.6 42.4 0 80.2 4.9 113.5 13.9 11.3-8.6 67.3-48.8 121.3-43.9 2.9 7.7 24.7 58.3 5.5 118 32.4 36.8 48.9 82.7 48.9 132.3 0 102.2-59 188.1-200 212.9a127.5 127.5 0 0138.1 91v112.5c.8 9 0 17.9 15 17.9 177.1-59.7 304.6-227 304.6-424.1 0-247.2-200.4-447.3-447.5-447.3z" />
+              </svg>
+            </a>
+            <a href="https://www.linkedin.com/in/nihal-devadiga-aa4218225/" target="_blank">
+              <svg
+                viewBox="0 0 1024 1024"
+                fill="currentColor"
+                className="w-8 h-8 text-blue-900 hover:text-teal-600"
+              >
+                <path d="M880 112H144c-17.7 0-32 14.3-32 32v736c0 17.7 14.3 32 32 32h736c17.7 0 32-14.3 32-32V144c0-17.7-14.3-32-32-32zM349.3 793.7H230.6V411.9h118.7v381.8zm-59.3-434a68.8 68.8 0 1168.8-68.8c-.1 38-30.9 68.8-68.8 68.8zm503.7 434H675.1V608c0-44.3-.8-101.2-61.7-101.2-61.7 0-71.2 48.2-71.2 98v188.9H423.7V411.9h113.8v52.2h1.6c15.8-30 54.5-61.7 112.3-61.7 120.2 0 142.3 79.1 142.3 181.9v209.4z" />
+              </svg>
+            </a>
+        
+            <a href="https://twitter.com/nihal_devadiga" target="_blank">
+              <svg
+                viewBox="0 0 1024 1024"
+                fill="currentColor"
+                className="w-8 h-8 text-blue-500 hover:text-teal-600"
+              >
+                <path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm215.3 337.7c.3 4.7.3 9.6.3 14.4 0 146.8-111.8 315.9-316.1 315.9-63 0-121.4-18.3-170.6-49.8 9 1 17.6 1.4 26.8 1.4 52 0 99.8-17.6 137.9-47.4-48.8-1-89.8-33-103.8-77 17.1 2.5 32.5 2.5 50.1-2a111 111 0 01-88.9-109v-1.4c14.7 8.3 32 13.4 50.1 14.1a111.13 111.13 0 01-49.5-92.4c0-20.7 5.4-39.6 15.1-56a315.28 315.28 0 00229 116.1C492 353.1 548.4 292 616.2 292c32 0 60.8 13.4 81.1 35 25.1-4.7 49.1-14.1 70.5-26.7-8.3 25.7-25.7 47.4-48.8 61.1 22.4-2.4 44-8.6 64-17.3-15.1 22.2-34 41.9-55.7 57.6z" />
+              </svg>
+            </a>
+            <a href="https://www.instagram.com/___nihahahaha___/" target="_blank">
+              <svg
+                fill="currentColor"
+                viewBox="0 0 17 17"
+                className="w-8 h-8 text-red-600 hover:text-teal-600"
+
+              >
+                <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 00-1.417.923A3.927 3.927 0 00.42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 001.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 00-.923-1.417A3.911 3.911 0 0013.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 01-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 01-.92-.598 2.48 2.48 0 01-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 100 1.92.96.96 0 000-1.92zm-4.27 1.122a4.109 4.109 0 100 8.217 4.109 4.109 0 000-8.217zm0 1.441a2.667 2.667 0 110 5.334 2.667 2.667 0 010-5.334z" />
+              </svg>
+            </a>
+          </div>
+        </div>
+
+        <div className="prose max-w-none prose-lg pt-8 pb-7 dark:prose-invert xl:col-span-2">
+          <p className=" font-mono">
+            Hey everyone my name is Nhal, I am 21 years old and i am a currently pursuing Bachelors in Computer Science Engineering in St Joseph Engineering College, Mangalore, Karnataka.
+          </p>
+          <p className=" font-mono">
+            This is my portfolio which will be guiding you through my blogs and my projects. If you wished to contact me, then do visit the contact section to get connected.
+          </p>
+          <br />
+          <h2 className=" font-extrabold">Hobbies</h2>
+          <p className=" font-mono">
+            I am a cricket and football enthusiastic. Die hard fan of Manchester City FC. Love playing sports (cricket,football,table tennis) at my free time. Along with watching sports, i like doing analysis on stats.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
